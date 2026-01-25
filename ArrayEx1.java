313ce800098986a3ee80bbc7e758ab23fc9d1f46
Sugish Devkota <inventyourshit7@gmail.com>
2026-01-26 05:26:10 +0545

    Add files via upload

---
+++ ArrayEx1.java
@@ -0,0 +1,16 @@
+//Simple OneDimensional Array. 
+public class ArrayEx1{
+    public static void main(String args[]){
+        int marks[] = new int[4];
+        marks[0]= 87;
+        marks[1]= 68;
+        marks[2]= 88;
+        marks[3]= 90;
+        // System.out.println(marks[3]); 
+        for(int i=0; i<4; i++){
+            System.out.println(marks[i]);
+        }
+
+    }
+
+}
