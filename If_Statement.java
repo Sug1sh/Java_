bddabfd27590348722887e4e933b3d8eba6c9db2
Sugish Devkota <inventyourshit7@gmail.com>
2026-01-27 09:34:44 +0545

    Add files via upload

---
+++ If_Statement.java
@@ -0,0 +1,12 @@
+public class If_Statement {
+     public static void main(String args[]){
+      
+
+    int sales=5000;
+    float commission=0.0f;
+   if(sales>3000)
+   commission=sales*0.5f;
+    System.out.println(commission);
+     }
+
+}
