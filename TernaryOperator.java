8ff393cf511410504d2b3f6b25e82e65a3fca28c
Sugish Devkota <inventyourshit7@gmail.com>
2026-01-27 19:24:04 +0545

    Add files via upload

---
+++ TernaryOperator.java
@@ -0,0 +1,9 @@
+public class TernaryOperator{
+    public static void main(String[] args) {
+        int a=10, b=20;
+        int Max;
+        Max = (a>b)? a:b;
+        System.out.println(Max);
+
+    }
+}
