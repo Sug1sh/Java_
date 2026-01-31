d85215eb6c1ab4d54d44fa131f898b60cbefbcca
Sugish Devkota <inventyourshit7@gmail.com>
2026-01-31 20:17:37 +0545

    Add files via upload

---
+++ Important_Simple_Java_Qns/Greatest_Among_Three.java
@@ -0,0 +1,33 @@
+package Important_Simple_Exam_Qn;
+
+import java.util.Scanner;
+
+public class Greatest_Among_Three{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the value of a: ");
+        int a= sc.nextInt();
+        System.out.println("Enter the value of b: ");
+        int b= sc.nextInt();
+        System.out.println("Enter the value of c: ");
+        int c= sc.nextInt();
+        if(a>b)
+        {
+            if(a>c){
+                System.out.println("a is greatest");              
+            }
+            else{
+                System.out.println("c is greatest");
+            }
+        }
+        else if(b>c){
+            System.out.println("b is greatest");
+        
+        }
+        else{
+            System.out.println("c is geatest");
+        }
+
+    }
+
+}
