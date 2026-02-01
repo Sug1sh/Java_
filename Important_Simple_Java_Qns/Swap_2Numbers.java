3c649fd458027a5955f600c6e1602f7a8a5697f7
Sugish Devkota <inventyourshit7@gmail.com>
2026-02-01 06:29:25 +0545

    Add files via upload

---
+++ Important_Simple_Java_Qns/Swap_2Numbers.java
@@ -0,0 +1,17 @@
+package Important_Simple_Exam_Qn;
+
+public class Swap_2Numbers {
+    public static void main(String[] args) {
+        int a=10, b=20;
+        System.out.println("Before swapping:");
+        System.out.println("a is "+a);
+        System.out.println("b is "+b);
+        int t;
+        t=a;
+        a=b;
+        b=t;
+        System.out.println("After Swapping: ");
+        System.out.println("a is "+a);
+        System.out.println("b is "+b);
+    }
+}
