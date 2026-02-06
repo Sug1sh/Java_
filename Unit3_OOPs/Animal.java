855d8aa369131245bd5067fe7481a2f31e593f02
Sugish Devkota <inventyourshit7@gmail.com>
2026-02-06 19:52:13 +0545

    Add files via upload

--- Unit3_OOPs/Animal.java
+++ Unit3_OOPs/Animal.java
@@ -1,1 +1,15 @@
+package Unit3_Oops;
 
+public class Animal{
+    public void eat()
+    {
+        System.out.println("murrah is eating grass");
+    }
+    public static void main(String[] args)
+     {
+        System.out.println("Hey bro lets learn OOPs");
+       Animal murrah = new Animal();
+       murrah.eat();
+
+    }
+}   
