02dede0761cd479204007a978ac1ff83dfc237ae
Sugish Devkota <inventyourshit7@gmail.com>
2026-03-15 19:48:27 +0545

    Update AirthmeticException.java

--- Unit5_Exception_Errors/AirthmeticException.java
+++ Unit5_Exception_Errors/AirthmeticException.java
@@ -3,8 +3,8 @@ package Unit5_Handling_Errors_Exception;
 public class AirthmeticException {
     public static void main(String[] args) {
         System.out.println("1");
-        System.out.println("2");
-        System.out.println(100/0);
+        System.out.println("2");    
+        System.out.println(100/0);    //java.lang.ArithmeticException.....
         System.out.println("3");
         System.out.println("4");
         System.out.println("5");
