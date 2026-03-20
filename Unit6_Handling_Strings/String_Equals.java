89e9abb6dda8aa55fccc46cde237b29626fdcdfd
Sugish Devkota <inventyourshit7@gmail.com>
2026-03-20 03:59:39 -0400

    Add files via upload

---
+++ Unit6_Handling_Strings/String_Equals.java
@@ -0,0 +1,13 @@
+package bca3rdsemjava.Unit6_String_Handling;
+
+public class String_Equals {
+    public static void main(String[] args) {
+        String s1= "Sugish";
+        String s2= "binaya";
+        String s3= "Binod";
+        String s4= "Binod";
+        String s5= "mohit";
+        System.out.println(s1.equals(s5));
+        System.out.println(s3.equals(s4));
+    }   
+}
