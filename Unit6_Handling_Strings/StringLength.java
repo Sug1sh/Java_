fd3ef120ccdcec215377792dc668c6a44a92ce84
Sugish Devkota <inventyourshit7@gmail.com>
2026-03-20 03:53:34 -0400

    Add files via upload

---
+++ Unit6_Handling_Strings/StringLength.java
@@ -0,0 +1,6 @@
+public class StringLength{
+    public static void main(String[] args) {
+        String s="Sugish Devkota";
+        System.out.println("The Length of the String is: "+s.length());
+    }
+}
