675c411572f7892f7b53d09d003a068d5f3f0a2a
Sugish Devkota <inventyourshit7@gmail.com>
2026-04-05 10:37:30 -0400

    Add files via upload

---
+++ Unit9_Understanding_Core_Packages/WrapperDemo.java
@@ -0,0 +1,16 @@
+
+/*Qn)Write a program to illustrate Integer and Double class as a wrapper class of int and double
+respectively.*/
+
+package Unit9_Understanding_Core_Packages;
+
+class WrapperDemo {
+    public static void main(String[] args) {
+        Integer a = 10;
+        System.out.println("Integer Value after converting the primitive to the wrapper class : " + a);
+        
+        Double b = 10.0;
+        System.out.println("Double Value after converting the primitive to the wrapper class: " + b);
+    }
+}
+
